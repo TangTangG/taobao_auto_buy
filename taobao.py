9d15f31ff880f84ad7bec5826672113079aacb0e
jesseslco <jesseslco@gmail.com>
2020-01-09 06:08:54 +0800

    adding new file

---
+++ taobao.py
@@ -0,0 +1,168 @@
+import logging
+import datetime
+import time
+from utils import Tools
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+class TaoBao(object):
+
+    num_instance = 0
+
+    def __init__(self, config):
+
+        self.browser = self.config_browser()
+        self.browser.maximize_window()
+        self.wait = WebDriverWait(self.browser,20)
+        
+        self.main_url = config["main_url"]      
+        self.target_url = config["target_url"]
+        self.amount_to_buy = config["amount_to_buy"];
+        self.plan_to_buy = config["plan_to_buy"];
+        self.prop_check_list = config["prop_check_list"]
+        self.buy_time = config["buy_time"]
+        
+        self.table = None
+
+    def login(self):
+
+        self.browser.get(self.main_url)
+        self.browser.find_element_by_link_text("亲，请登录").click()
+        current_url = self.browser.current_url
+        self.wait_redirect(current_url)
+
+    def start(self):
+
+        logging.info(f"Start")
+        self.login()
+        self.goto_detail()
+        self.get_table()
+        if self.table is not None:
+            self.input_form()
+            self.wait_for_shop()
+        else:
+            logging.error(f"error: -> can't get table")
+            self.close()
+
+    def get_table(self):
+
+        self.table = self.browser.find_element_by_xpath("//div[@class='tb-sku']")
+        
+    def wait_redirect(self, current_url):
+
+        try:
+            self.wait.until(lambda browser: browser.current_url != current_url)
+        except TimeoutException:
+            logging.warning(f"Timeout")
+
+    def config_browser(self):
+
+        opts = ChromeOptions()
+        opts.add_experimental_option("detach", True)
+        #opts.add_experimental_option("prefs", {"profile.managed_default_content_settings.images":2})
+        driver_dir = Tools.get_driver_dir()
+        return Chrome(driver_dir, chrome_options=opts)
+
+    def goto_detail(self):
+
+        self.browser.get(self.target_url)
+
+    def prop_check(self, table):
+        
+        props = table.find_elements_by_xpath("./dl[contains(@class, 'tm-sale-prop')]")
+
+        print(f"props: {len(props)}")
+        print(f"list: {self.prop_check_list}")
+        assert len(props) == len(self.prop_check_list)
+        logging.info("working right")
+
+        for i in zip(props, self.prop_check_list):
+            i[0].find_elements_by_xpath(".//li")[i[1]].click()
+
+    def amount_check(self, table):
+
+        amount = table.find_element_by_xpath("./dl[contains(@class, 'tb-amount')]").find_element_by_xpath(".//input[@type='text']")
+        amount.send_keys(Keys.CONTROL + "a")
+        amount.send_keys(Keys.DELETE)
+        amount.send_keys(self.amount_to_buy)
+
+    def plan_check(self, table):
+        
+        if self.plan_to_buy is None:
+            pass
+        else:
+            plans = table.find_element_by_id("J_Progressive")
+            plans.find_elements_by_xpath(".//li")[self.plan_to_buy].click()
+
+
+
+    def input_form(self):
+
+        self.prop_check(self.table)
+        self.amount_check(self.table)
+        self.plan_check(self.table)
+        
+    def wait_until_load(self, selector_text):
+
+        self.wait.until(EC.presence_of_all_elements_located(By.XPATH, selector_text))
+
+    def buy(self):
+
+        
+
+        current_url = self.browser.current_url
+        #self.wait_redirect(current_url)
+
+        while current_url == self.browser.current_url:
+            try:
+                self.table.find_element_by_id("J_LinkBuy").click()
+            except:
+                pass
+
+        self.sumit_order() 
+
+    def sumit_order(self):
+
+        current_url = self.browser.current_url
+
+        while current_url == self.browser.current_url:
+            try:
+                self.browser.find_element_by_id("submitOrderPC_1").find_element_by_xpath(".//a[@role='button']").click()
+            except:
+                pass
+
+    @classmethod
+    def count(cls):
+        
+        return cls.num_instance
+
+    def wait_for_shop(self):
+        
+       while 1:
+           now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+           Tools.time_distance(now, self.buy_time)
+           if now > self.buy_time:
+                self.buy()
+            
+           #time.sleep(0.1)
+        
+    def close(self):
+        self.browser.close()
+
+def main():
+
+    logging.basicConfig(level=logging.INFO)
+    config = Tools.load_config_json()
+
+    if config is not None:
+        taobao = TaoBao(config)
+        taobao.start()
+    else:
+        logging.warning("please input info to config.json")
+
+if __name__ == "__main__":
+    main() 
---
+++ utils.py
@@ -0,0 +1,68 @@
+import platform
+import os
+import json
+import sys
+from dateutil import parser as date_parse
+if "logging" not in sys.modules:
+    print("can't use module logging")
+else:
+    import logging
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.DEBUG) 
+
+class Tools(object):
+
+    base_config_json = {
+                            "main_url" : "https://www.taobao.com/",
+                            "target_url" : "https://detail.tmall.com/item.htm?spm=a1z10.4-b-s.w4007-21704868460.13.2d2d4d36ktntw0&id=607814862914",
+                            "amount_to_buy" : 1,
+                            "plan_to_buy" : None,
+                            "prop_check_list" : [1, 2, 3],
+                            "buy_time" : "2020-01-08 10:27:23", 
+                       }
+
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
+
+    @staticmethod
+    def get_driver_dir():
+        
+        os_type = platform.system()
+        drivers_dir = os.path.join(Tools.base_dir, "drivers")
+
+        if os_type == "Linux":
+            return os.path.join(drivers_dir, "chromedriver")
+        elif os_type == "Darwin":
+            return os.path.join(drivers_dir, "chromedriver_mac64")
+        elif os_type == "Windows":
+            return os.path.join(drivers_dir, "chromedriver_win32.exe")
+
+        return None
+
+    @staticmethod
+    def load_config_json():
+
+        config_json_dir = os.path.join(Tools.base_dir, "config.json") 
+
+        if os.path.isfile(config_json_dir) and os.access(config_json_dir, os.R_OK):
+            with open(config_json_dir, "r") as f:
+                config = json.load(f)
+            return config
+        else:
+            logging.warning(f"config file not existed or could not access it")
+            logging.info(f"create default config.json")
+            Tools.create_template_config_json(config_json_dir)
+            return None
+            
+    @staticmethod
+    def create_template_config_json(file_path: str) -> None:
+
+        with open(file_path, "w") as f:
+            json.dump(Tools.base_config_json, f, indent = 4)
+
+    @staticmethod
+    def time_distance(current: str, target: str):
+        logging.info(f"{date_parse.parse(target) - date_parse.parse(current)}")
+
+        
